41ae96f98cd6389ddb15eabb42c7f2db63c38a3a
颖倩 侯 <HYQ@appledeMacBook-Pro-2.local>
2015-05-01 13:46:47 +0800

    initial commit

---
+++ BaiKe/test0/test0/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test0
+//
+//  Created by 颖倩 侯 on 15-5-1.
+//  Copyright (c) 2015年 颖倩 侯. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
